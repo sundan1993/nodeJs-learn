36cd1b20ca54f9dfb8b7a6d7971e4e886c90ea63
孙丹 <sundan@sundandeMacBook-Air.local>
2017-02-05 19:58:05 +0800

    测试提交

--- hello.js
+++ hello.js
@@ -12,6 +12,7 @@
 // 相对路径必须写./不然就是加载核心模块或者是node_modules内的文件
 // 文件查找机制：先找文件名，后找.js再.json再.node，再报错
 // console.log(module)
+
 var m5 = require('./modules/test.js');
 // 在这个global对象下有一个exports对象。
  console.log( m5)
