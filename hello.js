8e6d876b9a0c0bcd0911f9d8f18163af79148187
孙丹 <sundan@kaike.la>
2017-02-06 09:08:37 +0800

    0170206

--- hello.js
+++ hello.js
@@ -12,6 +12,8 @@
 // 相对路径必须写./不然就是加载核心模块或者是node_modules内的文件
 // 文件查找机制：先找文件名，后找.js再.json再.node，再报错
 // console.log(module)
-var m5 = require('./modules/test.js');
+// var m5 = require('./modules/test.js');
 // 在这个global对象下有一个exports对象。
- console.log( m5)
+//  console.log( m5)
+
+console.log(__filename)
