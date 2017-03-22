4474353753a0a1d1e29ddc2e0de04c08455c9d58
孙丹 <sundan@kaike.la>
2017-03-22 18:02:09 +0800

    20170322提交

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// var http = require('http');
+// http.createServer(function(req,res){
+// 	res.writeHead(200, {'Content-Type': 'text/html'});
+// 	res.write('<h1>Node.js</h1>');
+// 	res.end('<p>hello world!!!!</p>');
+// }).listen(3000);
+
+
+// console.log("HTTP server is listening at port 3000.");
+
 var http = require('http');
-http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<h1>Node.js</h1>');
-	res.end('<p>hello world!!!!</p>');
+var url = require('url');
+var util = require('util');
+http.createServer(function(req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end(util.inspect(url.parse(req.url, true)));
 }).listen(3000);
-
-
-console.log("HTTP server is listening at port 3000.");
