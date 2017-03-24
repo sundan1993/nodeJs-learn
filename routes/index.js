ac2ee24510395aa4ab74a629e5eb7e4a1833c19f
孙丹 <sundan@kaike.la>
2017-03-24 18:01:14 +0800

    20170324提交

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+		title: 'Express',
+		items:[12, 33, 44, 55] 
+	});
 });
 
 module.exports = router;
--- routes/time.js
+++ routes/time.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.send('The time is ' + new Date().toString());
+router.get('/:day', function(req, res, next) {
+	res.send('The time is ' + new Date().toString() + 'day' + req.params.day);
 });
 
 module.exports = router;
